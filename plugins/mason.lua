9d00cd5ecb421dca65476fcead9893f4876be902
lamkimphu258 <lamkimphu258@gmail.com>
2024-02-18 18:47:20 +0700

    remove blade formatter in null ls

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -38,7 +38,6 @@ return {
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         -- "prettier",
         -- "stylua",
-        "blade_formatter"
       })
     end,
   },
